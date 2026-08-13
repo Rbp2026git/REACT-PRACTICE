e2a8704b8bed322bdf20d2e727a3d4071e28b7b2
Raushan Bhai Patel <rbpmicrosoft2025@gmail.com>
2026-08-13 18:54:07 +0530

    initial repo

---
+++ react-practice/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Raushan Bhai Patel</h1>
+      <p>This is my first <b>REACT Environmental Setup</b>.</p>
+    </>
+  )
+}
+
+export default App
