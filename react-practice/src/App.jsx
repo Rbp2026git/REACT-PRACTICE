67a65b21878f4a8768c9bb20461ae767d15c2cc4
Raushan Bhai Patel <rbpmicrosoft2025@gmail.com>
2026-08-14 16:24:17 +0530

    first commit

--- react-practice/src/App.jsx
+++ react-practice/src/App.jsx
@@ -3,16 +3,28 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import Container from './Boxes.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [name, setName] = useState("")
 
-  return (
+  return ( 
     <>
-      <h1>Raushan Bhai Patel</h1>
-      <p>This is my first <b>REACT Environmental Setup</b>.</p>
+      <h1>UserName : {name}</h1>
+      <input type="text" placeholder='Enter your name' onChange={(e) => setName(e.target.value)} />
+      <p>Count : {count}</p>
+      <button onClick={() => setCount(count + 1)}>Click Me</button>
+
+      <Container />
+      <div>{profile("Adrina Patel", 24)}</div>
     </>
   )
+}
+
+function profile(name, age){
+  console.log(`My name is ${name} and ${age} years old.`);
+  return `My name is ${name} and ${age} years old.`;
 }
 
 export default App
---
+++ react-practice/src/Boxes.jsx
@@ -0,0 +1,12 @@
+function Container(){
+    return(
+        <div className="boxes">
+            <div style={{ backgroundColor: "red", width: "50px", height: "50px", border: "1px solid black", marginTop: "5px" }}>Box1</div>
+            <div style={{ backgroundColor: "blue", width: "50px", height: "50px", border: "1px solid black", marginTop: "5px" }}>Box2</div>
+            <div style={{ backgroundColor: "green", width: "50px", height: "50px", border: "1px solid black", marginTop: "5px" }}>Box3</div>
+            <div style={{ backgroundColor: "yellow", width: "50px", height: "50px", border: "1px solid black", marginTop: "5px" }}>Box4</div>
+            <div style={{ backgroundColor: "purple", width: "50px", height: "50px", border: "1px solid black", marginTop: "5px" }}>Box5</div>
+        </div>
+    )
+}
+export default Container;
