e85431484c24b5e88a96efc08f2d8a953a3d8079
Raushan Bhai Patel <rbpmicrosoft2025@gmail.com>
2026-08-19 22:51:07 +0530

    adding

---
+++ react-practice-06/Props-Understanding/src/UserCard.jsx
@@ -0,0 +1,32 @@
+function UserCard (Props) {
+    return (
+        <div>
+            <h2>{Props.name}</h2>
+            <p>{Props.age}</p>
+            <hr />
+        </div>
+    )
+}
+
+export default UserCard
+
+
+// export function UserCard2 () {
+//     return (
+//         <div>
+//             <hr />
+//             <h2>Adrina</h2>
+//             <p>Age: 24</p>
+//         </div>
+//     )
+// }
+
+// export function UserCard3 () {
+//     return (
+//         <div>
+//             <hr />
+//             <h2>Archi</h2>
+//             <p>Age: 21</p>
+//         </div>
+//     )
+// }
