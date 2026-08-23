6a2ce2fd788c14a5d36278573ff1779d9b1637ca
Raushan Bhai Patel <rbpmicrosoft2025@gmail.com>
2026-08-23 12:27:23 +0530

    Complete props revision

--- react-practice-06/Props-Understanding-02/src/App.jsx
+++ react-practice-06/Props-Understanding-02/src/App.jsx
@@ -2,13 +2,15 @@ import User from './User';
 import userData from './data';
 import UserCard from './components/nested-props/UserCard';
 import TopScores, {PlayerList} from "./components/array-props/TopScores";
+import Layout from './components/children-props/Layout';
+
 
 function App() {
 
 
   return (
     <>
-      {userData.map((user, index) => (
+      {/* {userData.map((user, index) => (
         <User
           key={index}
           name={user.name}
@@ -28,7 +30,42 @@ function App() {
       <TopScores scores = {[95, 88, 76, 66]} />
       <hr />
 
-      <PlayerList players = {["Virat", "Dhoni", "Bumrah", "Suman"]} />
+      <PlayerList players = {["Virat", "Dhoni", "Bumrah", "Suman"]} /> */}
+
+      <Layout>
+        <h3>React</h3>
+        <p>Learn React from scratch</p>
+        <h2>Raushan Bhai Patel</h2>
+      </Layout>
+      <hr />
+
+      <Layout>
+        Adrina Patel
+      </Layout>
+      <Layout>
+        <ul>
+          <li>Virat</li>
+          <li>Rohit</li>
+          <li>Dhoni</li>
+          <li>Suman</li>
+        </ul>
+        
+      </Layout>
+
+      <hr />
+      <Layout>
+        {userData.map((user, index) => (
+        <User
+          key={index}
+          name={user.name}
+          age={user.age}
+          email={user.email}
+          city={user.city}
+          state={user.state}
+        />
+      ))}
+      </Layout>
+
 
     </>
   )
